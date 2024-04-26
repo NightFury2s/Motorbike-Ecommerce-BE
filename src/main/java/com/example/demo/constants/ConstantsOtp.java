91890855fd717789ffb30cf3dba6b283656cf5fd
DatNguyen1231 <129192090+DatNguyen1231@users.noreply.github.com>
2024-04-26 08:25:25 +0700

    update

---
+++ src/main/java/com/example/demo/constants/ConstantsOtp.java
@@ -0,0 +1,11 @@
+package com.example.demo.constants;
+
+public class ConstantsOtp {
+    public static final String INVALID_EMAIL = "Địa chỉ email không đúng hoặc không tồn tại.";
+    public static final String SEND_OTP_UCCESSFULLY = "Mã OTP đã được đặt lại và thông báo đã được gửi đến email của bạn.";
+    public static final String ERROR_SEND_OTP = "Có lỗi xảy ra";
+    public static final String WRONG_OTP = "Mã xác nhận không đúng. ";
+    public static final String EXPIRED_OTP = "Mã xác nhận đã hết hạn . ";
+    public static final String SEND_PASSWORD_SUCCESSFULLY = "Mã OTP đã được đặt lại và thông báo đã được gửi đến email của bạn.";
+
+}
---
+++ src/main/java/com/example/demo/constants/ConstantsShoppingCart.java
@@ -0,0 +1,14 @@
+package com.example.demo.constants;
+
+public class ConstantsShoppingCart {
+    public static final String QUANTITY_GREATER_THAN_ZERO = "Số lượng phải lớn hơn 0. ";
+    public static final String PRODUCT_NOT_FOUND = "Sản phẩm không tồn tại";
+    public static final String QUANTITY_LIMIT_REACHED = "Số lượng đã đến giới hạn. ";
+    public static final String ADD_SUCCESS = "Thêm vào giỏ hàng thành công";
+    public static final String ADD_ERROR = "Lỗi khi thêm vào giỏ hàng";
+    public static final String CART_EMPTY = "Giỏ hàng rỗng. ";
+    public static final String DELETE_SUCCESS = "Xóa sản phẩm thành công";
+    public static final String PRODUCT_NOT_EXIST = "Sản phẩm không tồn tại";
+    public static final String UPDATE_SUCCESS = "Cập nhật thành công";
+    public static final String UPDATE_ERROR = "Lỗi khi cập nhật giỏ hàng";
+}
